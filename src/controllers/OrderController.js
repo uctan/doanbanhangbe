f59da509c340eb0d069c416bdadbd56b63cde116
uctan <duytan20032015@gmail.com>
2024-09-06 10:14:05 +0700

    update shippingfee

--- src/controllers/OrderController.js
+++ src/controllers/OrderController.js
@@ -3,12 +3,13 @@ const OrderService = require('../service/OrderService')
 const createOrder = async (req, res) => {
     try { 
         const { paymentMethod, itemsPrice, shippingPrice, totalPrice, fullName, address, city, phone } = req.body
-        if (!paymentMethod || !itemsPrice || !shippingPrice || !totalPrice || !fullName || !address || !city || !phone) {
+        if (!paymentMethod || !itemsPrice || shippingPrice || !totalPrice || !fullName || !address || !city || !phone) {
             return res.status(200).json({
                 status: 'ERR',
-                message: 'The input is required'
+                message: 'The input is required '
             })
         }
+     
         const response = await OrderService.createOrder(req.body)
         return res.status(200).json(response)
     } catch (e) {
@@ -17,6 +18,8 @@ const createOrder = async (req, res) => {
         })
     }
 }
+
+
 const  getAllOrderDetails  = async (req,res) => {
     try {
         const userId = req.params.id
